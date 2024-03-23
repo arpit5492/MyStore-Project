a98c05d71155c0c945bf84dd646f1d2a890fcb8b
arpit5492 <arpitmallick99@gmail.com>
2024-03-22 20:35:35 -0700

    Adding and show products working fine

--- controllers/prodController.js
+++ controllers/prodController.js
@@ -1,31 +1,37 @@
 // import {fetchProducts, postData, updateData, delData} from "../db/product.js";
+import { Product } from "../db/product.js";
 
 // getting all the products
 const getAllProds = async (req, res) => {
-    const products = await fetchProducts();
-    res.render("home", {title: "Home", products: products, isLoggedIn: global.isLoggedIn});
-};
-
-const editProds = async (req, res) => {
-    const [products] = await fetchProducts();
-    // console.log(products);
-    // console.log(cookie);
-    res.render("editProd", {title: "Edit Product", prod: products, isLoggedIn: global.isLoggedIn});
-};
-
-const updateEachProd = async (req, res) => {
-    // console.log(req.body);
-    const {prod_name, price} = req.body;
-    const img = req.file.filename;
-    const id = req.params.id;
-    // console.log(id, prod_name, price, image);
     try{
-        await updateData(id, prod_name, price, img);
-        res.redirect("/");
-    } catch (err) {
+        const products = await Product.findAll();
+        console.log(JSON.stringify(products, null, 2));
+        res.render("home", {title: "Home", products: products, isLoggedIn: global.isLoggedIn});
+    } catch(err) {
         console.log(err);
     }
 };
+
+// const editProds = async (req, res) => {
+//     const [products] = await fetchProducts();
+//     // console.log(products);
+//     // console.log(cookie);
+//     res.render("editProd", {title: "Edit Product", prod: products, isLoggedIn: global.isLoggedIn});
+// };
+
+// const updateEachProd = async (req, res) => {
+//     // console.log(req.body);
+//     const {prod_name, price} = req.body;
+//     const img = req.file.filename;
+//     const id = req.params.id;
+//     // console.log(id, prod_name, price, image);
+//     try{
+//         await updateData(id, prod_name, price, img);
+//         res.redirect("/");
+//     } catch (err) {
+//         console.log(err);
+//     }
+// };
 
 const renderAddProd = (req, res) => {
     // console.log(cookie);
@@ -36,21 +42,25 @@ const postAddProd = async (req, res) => {
     const {prod_name, price} = req.body;
     const img = req.file.filename;
     try{
-        await postData(prod_name, price, img);
+        await Product.create({
+            pName: prod_name,
+            price: price,
+            image: img
+        });
         res.redirect("/");
     } catch(err) {
         console.log(err);
     }
 };
 
-const delProd = async (req, res) => {
-    const id = req.params.id;
-    try{
-        await delData(id);
-        res.redirect("/");
-    } catch(err) {
-        console.log(err);
-    }
-};
+// const delProd = async (req, res) => {
+//     const id = req.params.id;
+//     try{
+//         await delData(id);
+//         res.redirect("/");
+//     } catch(err) {
+//         console.log(err);
+//     }
+// };
 
-export {getAllProds, editProds, renderAddProd, postAddProd, updateEachProd, delProd};
+export {getAllProds, /*editProds*/ renderAddProd, postAddProd /*updateEachProd*/ /*delProd*/};
